cf76363412f3684079671249b7c74af3604bb8eb
Vicent G <gilabert_vicent@hotmail.com>
2022-03-23 19:39:42 +0100

    [MDF] Modificar testing

--- test.py
+++ test.py
@@ -1,19 +1,33 @@
 import unittest
-
+import ImperDetect as ID
 
 class projectTesting (unittest.TestCase):
     def check_input_image(self):
         '''
             Test input image. 
         '''
+        file = 'dataset/IMAGES/inclusion_1.jpg'
         img = cv2.imread(file)
-        h, w = img.shape
+        size = img.shape
+
+        self.assertEqual(size, (200,200))
 
     
-    def check_result(self):
+    def check_result_label(self):
         '''
             Test result of system. 
         '''
+        file = 'dataset/IMAGES/inclusion_1.jpg'
+        img = cv2.imread(file)
+        label, _ = ID.classify_and_locate()
+        self.assertEqual(?)
 
-
+    def check_result_location(self):
+        '''
+            Test result of system. 
+        '''
+        file = 'dataset/IMAGES/inclusion_1.jpg'
+        img = cv2.imread(file)
+        _, location = ID.classify_and_locate()
+        self.assertEqual(?)
     
